04554053f54e029b340defc8afb004cec75c59c2
Samplezt <lucasgriotti@hotmail.com>
2022-03-09 15:27:55 -0300

    modifico clase home para que maneje la aparicion de publicidades

--- page-objects/pages/home.js
+++ page-objects/pages/home.js
@@ -7,6 +7,7 @@ class HomePage {
         this.mainLogo           = Selector('div.site-logo')
         this.searchBox          = Selector('input#search-key')
         this.searchButton       = Selector('input.search-button')
+        this.popup              = Selector('div.poplayer-content')
     }
 
     async search(product){
@@ -16,6 +17,13 @@ class HomePage {
         await t
             .click(this.searchButton)
         
+    }
+
+    async managePopup(){
+        if (await this.popup.visible){
+            await t.click('img.btn-close')
+        }
+
     }
 
 
