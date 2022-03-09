32e2ac0a829777fee9ce2fa1ac6f70e1515b43c9
Samplezt <lucasgriotti@hotmail.com>
2022-03-09 15:31:09 -0300

    creo el fixture con el test de verificacion de stock

---
+++ tests/product.search.test.js
@@ -0,0 +1,26 @@
+import {t, Selector, RequestLogger} from 'testcafe';
+import dataQa from '../utils/test-data.json'
+import HomePage from '../page-objects/pages/home'
+import ListingPage from '../page-objects/pages/listing'
+import Product from '../page-objects/pages/product'
+
+fixture `Busqueda de productos`
+     .page `${dataQa.mainUrl}`
+     .beforeEach(async t => {
+          await t.maximizeWindow() 
+          await t.expect(HomePage.mainLogo.visible).ok() // verificamos la carga de la pagina
+      })
+     
+test("El segundo item debe tener stock para la compra", async t =>{
+    await HomePage.managePopup()
+    await HomePage.search('iphone')
+    await ListingPage.clickNextPage()
+    await ListingPage.clickOnProduct(2)
+    //await t.expect((Selector('div.product-quantity-tip')).exists).ok()
+    await t.expect(Product.stockAvailable.exists).ok() // esperamos a que sea accesible el elemento que tiene el stock disponible
+    //let currentStock = await (Selector('div.product-quantity-tip')).textContent
+    let currentStock = await (Product.stockAvailable).textContent
+    currentStock = currentStock.split(" ")
+    currentStock = parseInt(currentStock[0])
+    await t.expect(currentStock).gte(1) // verificamos que haya stock disponible para la compra
+})
